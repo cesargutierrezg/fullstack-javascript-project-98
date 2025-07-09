ae308881344fad8e1af9f048100468041ecdb6eb
cesargutierrezg <ovedgu@hotmail.com>
2025-07-08 21:37:22 -0500

    tercera revicion editado 3

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 const saludo = () => {
     console.log('¡Bienvenido a Brain Games!');
     const name = readlineSync.question('¿Cuál es tu nombre? ');
-    console.log(`Hello, ${name}!`);
+    console.log(`Hola, ${name}!`);
     return name;
 }
 
